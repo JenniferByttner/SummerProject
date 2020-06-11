dd68e58ccaf5644499c595933540e1e2d566df53
JenniferByttner <jenniferbyttner@gmail.com>
2020-06-11 13:31:18 +0200

    added gitignore, merge header, home, logoBML and storys .js

--- src/Header.js
+++ src/Header.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import LogoBML from './LogoBML.svg';
 import './App.css';
@@ -23,3 +24,30 @@ export function Header() {
 
 export default Header;
 
+=======
+import React from 'react';
+import LogoBML from './LogoBML.svg';
+import './App.css';
+import {
+    Link
+  } from "react-router-dom";
+
+
+export function Header() {
+  return (
+    <div className="App">
+      <header className="header">
+        <img src={LogoBML} className="App-logo" alt="logo" />
+        <nav className="header">
+        <p><Link className="Menu-link" to="/">Home</Link></p>
+        <p><Link className="Menu-link"  to="/about">About</Link></p>
+        <p><Link className="Menu-link" to="/users">Users</Link></p>
+        </nav>       
+      </header>
+    </div>
+  );
+}
+
+export default Header;
+
+>>>>>>> Stashed changes
